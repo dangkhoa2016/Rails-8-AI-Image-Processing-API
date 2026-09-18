663948b5a38537c7a96b558e52887b84da7a4eeb
Đăng Khoa <i.am@dangkhoa.dev>
2026-09-18 08:37:04 +0000

    test(docker): align policy with lockfile build
    - Require the production Dockerfile test to verify Gemfile.lock is copied before bundle install.
    - Remove stale assertions that prohibited committed-lockfile Docker builds.

--- test/lib/dockerfile_policy_test.rb
+++ test/lib/dockerfile_policy_test.rb
@@ -3,10 +3,14 @@
 require "test_helper"
 
 class DockerfilePolicyTest < ActiveSupport::TestCase
-  test "build does not require a committed lockfile" do
+  test "copies the committed lockfile before bundle install" do
     dockerfile = Rails.root.join("Dockerfile").read
-    assert_includes dockerfile, "COPY Gemfile ./"
-    refute_match(/COPY\s+Gemfile\s+Gemfile\.lock/, dockerfile)
+    copy_position = dockerfile.index("COPY Gemfile Gemfile.lock ./")
+    bundle_position = dockerfile.index("RUN bundle install")
+
+    refute_nil copy_position
+    refute_nil bundle_position
+    assert_operator copy_position, :<, bundle_position
   end
 
   test "uses the documented default Ruby" do
@@ -19,7 +23,6 @@ class DockerfilePolicyTest < ActiveSupport::TestCase
     assert_includes dockerfile, "libpq5"
     assert_includes dockerfile, "libpq-dev"
     refute_match(/apt-get install.*\bsqlite3\b/, dockerfile)
-    refute_match(/COPY\s+Gemfile\s+Gemfile\.lock/, dockerfile)
     assert_includes dockerfile, 'ENV BUNDLE_DEPLOYMENT="1"'
   end
 end
